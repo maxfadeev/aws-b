fcd3c34b574f7e4e6b41227a5d18271ce1d7708c
maxfadeev <oxinhood@gmail.com>
2016-10-21 17:35:27 +0300

    Add the comment form

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: './public/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: './public/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
 };
