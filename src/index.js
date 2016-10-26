690319111c17b542b1d9c4f567ba6655bb3d4063
maxfadeev <masdeft@gmail.com>
2016-10-26 19:44:34 +0300

    Change a resource invoke url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import CommentBox from './comment-box';
 
 ReactDOM.render(
-    <CommentBox source="https://x1026y9qgg.execute-api.eu-central-1.amazonaws.com/test/comments" />,
+    <CommentBox source="https://6k0g49a2v2.execute-api.eu-central-1.amazonaws.com/blog/comments" />,
     document.getElementById('content')
 );
